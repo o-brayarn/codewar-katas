c67cd5366235f5f5b7c8286f4c98f3ced925e94b
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-06-09 15:44:06 +0300

    kata instructions

--- 8-kyu/calculate-average.js
+++ 8-kyu/calculate-average.js
@@ -1,3 +1,10 @@
+/*
+INSTRUCTIONS:
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+*/
+
 function findAverage(array) {
   if (array.length) {
     return array.reduce((a, b) => a + b) / array.length;
