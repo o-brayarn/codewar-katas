272ea93d8fb5fd0cc71830262dbba8e89754774a
Brayarn Ogeda <brayarnogeda@gmail.com>
2023-06-09 15:40:00 +0300

    calculate average

---
+++ 8-kyu/calculate-average.js
@@ -0,0 +1,17 @@
+function findAverage(array) {
+  if (array.length) {
+    return array.reduce((a, b) => a + b) / array.length;
+  }
+  return 0;
+}
+findAverage([1, 2, 3, 5, 6]);
+
+//  Alternative solution
+function findAverage(array) {
+  return array.length ? array.reduce((a, b) => a + b) / array.length : 0;
+}
+
+findAverage([1, 2, 3]);
+
+// You can also use forEach loop, map () method, for loop, and for/of loop
+// to first find the sum of the array, then divide with the array length
